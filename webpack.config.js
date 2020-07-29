eb868d7de3aaa6b9e10860c7685a5201026f7fd7
Adam Kukliński <64474051+Q-Click@users.noreply.github.com>
2020-07-29 13:20:16 +0200

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./js/app.js",
+
+  mode: "development",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    publicPath: "/",
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png|jpeg)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.mp3$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
